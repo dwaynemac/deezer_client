39aa05a75c15cb890484f6c9aea41db22847073b
Dwayne Macgowan <dwaynemac@gmail.com>
2013-06-18 02:22:30 -0300

    complete artist, album and track attributes list
    - also upgraded to logical_model 0.5.2

--- app/models/deezer/album.rb
+++ app/models/deezer/album.rb
@@ -1,13 +1,23 @@
 module Deezer
+  # API Doc: http://developers.deezer.com/api/album
   class Album < LogicalModel
     set_resource_url 'api.deezer.com/2.0', '/album'
     force_ssl
 
-    attribute :id
-    attribute :title
-    attribute :link
-    attribute :cover
-    attribute :release_date
+    attribute :id             # The album's Deezer id
+    attribute :title          # The album's title
+    attribute :link           # The url of the album on Deezer
+    attribute :cover          # The url of the album's cover. Add 'size' parameter to the url to change size. Can be 'small', 'medium', 'big'
+    attribute :genre_id       # The album's genre id
+    attribute :label          # The album's label name
+    attribute :duration       # The album's duration (seconds)
+    attribute :fans           # The number of album's Fans
+    attribute :rating         # The playlist's rate
+    attribute :release_date   # The album's release date
+    attribute :available
+    attribute :artist         # artist object containing : id, name, picture
+
+    has_many :tracks
 
   end
 end
--- app/models/deezer/artist.rb
+++ app/models/deezer/artist.rb
@@ -1,15 +1,16 @@
 module Deezer
+  # API Doc: http://developers.deezer.com/api/album
   class Artist < LogicalModel
     set_resource_url 'api.deezer.com/2.0', '/artist'
     force_ssl
 
-    attribute :id
-    attribute :name
-    attribute :link
-    attribute :picture
-    attribute :nb_album
-    attribute :nb_fan
-    attribute :radio
+    attribute :id         # The artist's Deezer id
+    attribute :name       # The artist's name
+    attribute :link       # The url of the artist on Deezer
+    attribute :picture    # The url of the artist picture. Add 'size' parameter to the url to change size. Can be 'small', 'medium', 'big'
+    attribute :nb_album   # The number of artist's albums
+    attribute :nb_fan     # The number of artist's fans
+    attribute :radio      # true if the artist has a smartradio
 
   end
 end
